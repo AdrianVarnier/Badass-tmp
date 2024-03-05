44df6d0fd16d95948a697ef8ad76fbb3531a0fca
avarnier <adrian.varnier@proton.me>
2024-03-05 14:20:41 +0100

    install p1

---
+++ P1/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Update and Upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install Docker
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Install GNS3
+sudo add-apt-repository ppa:gns3/ppa
+sudo apt update -y                                
+sudo apt install gns3-gui gns3-server
+
+# Install Gnome-session-flashback
+sudo apt install gnome-session-flashback
